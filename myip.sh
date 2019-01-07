252b6c5b6c0c4885649f9629092a0247c0d3aef1
Thiago Fonseca Born da Silva <thiagofborn@gmail.com>
2019-01-07 11:57:05 +0200

    Adding some extra info

--- myip.sh
+++ myip.sh
@@ -3,12 +3,15 @@
 #set -o errexit
 #set -o pipefail
 #
-# Description: 
+# Description: Every room gets a different wifi, so I am running this to change and keep "myip" as my
+# hostname.
+# On my /etc/hosts I have the line: 10.222.108.37 myip 784f435de58f.ant.hehehe.com
+# It is Mac OS. Network interface en0
+# 
 # Author: Thiago Born. e-mail: thiagofborn@gmail.com
 #------------------------------------------------------
-
 ip=`ifconfig en0 | grep inet  | grep -v inet6 | awk '{print $2}'`
-name=myip 
+name=myip
 export ip myip
 ipname="$ip $name"
 
